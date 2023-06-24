52a27bd2b3e93ff712f6e6c429f0c9aea8ca5a76
ssuj <dbsdmsdud912@gmail.com>
2023-06-24 21:29:02 +0900

    이미지 업로드 시 검사 추가

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -10,5 +10,15 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage }); // 업로드된 파일이 어떻게 저장될지를 지정
+// 파일 필터 함수: 이미지 파일 여부를 검사하는 로직
+const fileFilter = function (req, file, callback) {
+  // 이미지 파일인지 MIME 타입으로 검사
+  if (file.mimetype.startsWith("image/")) {
+    callback(null, true); // 이미지 파일인 경우 허용
+  } else {
+    callback(new Error("Only image files are allowed!"), false); // 이미지 파일이 아닌 경우 에러 발생
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 module.exports = upload;
